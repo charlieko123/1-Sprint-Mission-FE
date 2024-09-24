20f2c9b1b3791b26659ded5a26ae9ee3439ea6eb
jongmin <charlieko123@github.com>
2024-09-24 17:18:53 +0900

    :white_check_mark:[Test] create default items page

---
+++ pages/items.jsx
@@ -0,0 +1,9 @@
+import styles from "@styles/Items.module.css";
+
+export default function Items() {
+  return (
+    <div className={styles.itemsContainer}>
+      <h1 className={`text-xl bold`}>Items page 입니다</h1>
+    </div>
+  );
+}
