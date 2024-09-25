20a2c340ee2dc6bc392799d05fd9ee4d07e76cdb
jongmin <charlieko123@github.com>
2024-09-25 11:34:10 +0900

    :wrench:[Chore] allow all domain for image

---
+++ next.config.js
@@ -0,0 +1,13 @@
+// 모든 도메인 허용
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
