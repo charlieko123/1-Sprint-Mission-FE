9ace9212b6459f53f2fedd0eff76d4862ddc09db
jongmin <charlieko123@github.com>
2024-11-05 16:27:42 +0900

    페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,14 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/landing/LandingPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/landing/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const LandingPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to the Landing Page!</h1>
+      <p>This is the main page of our application.</p>
+    </div>
+  );
+};
+
+export default LandingPage;
