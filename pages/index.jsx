09882a3956a98facac7290c5e854dcc4b0a0c43f
jongmin <charlieko123@github.com>
2024-11-05 10:11:02 +0900

    임시 저장

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,7 +5,6 @@ export default function Home() {
   return (
     <div className={styles.homeContainer}>
       <BestProductsMain />
-      <h1 className={`${styles.homeTitle} text-xl bold`}>Home Page 입니다</h1>
     </div>
   );
 }
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,26 +1,32 @@
 import React from "react";
+import styles from "@styles/Product.module.css";
+import Image from "next/image";
+
 import heartEmptyImage from "../images/favoriteEmptyHeart-small.png";
-import defaultPanda from "../images/panda_image.png";
-import "../styles/Product.css";
+import defaultPanda from "@images/panda_image.png";
 
 const Product = ({ product, className }) => {
   const favoriteCountDefault = 1;
   return (
-    <div className={`${className}`}>
-      <img
+    <div>
+      <Image
         className="product-image"
         src={defaultPanda}
         alt={product.description}
+        width={282}
+        height={282}
       />
       <p className="description text-md medium">{product.description}</p>
       <p className="price text-lg bold">
         {product.price.toLocaleString("en-US")}원
       </p>
       <div className="favorites">
-        <img
+        <Image
           className="favorite-heart"
           src={heartEmptyImage}
           alt="favorite heart"
+          width={16}
+          height={16}
         />
         <p className="favorite-count text-xs medium">{favoriteCountDefault}</p>
       </div>
